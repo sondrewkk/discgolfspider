17b096a630441812df37abc74a818457d85e1b60
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2021-04-11 02:57:59 +0200

    Better if statement readabillity

--- discgolfspider/spiders/guru_spider.py
+++ discgolfspider/spiders/guru_spider.py
@@ -5,58 +5,12 @@ import scrapy
 class GuruSpider(scrapy.Spider):
   name = "guru"
   allowed_domains = ["gurudiscgolf.no"]
-  # start_urls = [
-  #   "https://gurudiscgolf.no/diskgolf/disctroyer.html",
-  #   "https://gurudiscgolf.no/diskgolf/infinite-discs/d-blend.html",
-  #   "https://gurudiscgolf.no/diskgolf/infinite-discs/g-blend.html",
-  #   "https://gurudiscgolf.no/diskgolf/infinite-discs/i-blend.html",
-  #   "https://gurudiscgolf.no/diskgolf/infinite-discs/p-blend.html",
-  #   "https://gurudiscgolf.no/diskgolf/infinite-discs/x-blend.html",
-  #   "https://gurudiscgolf.no/diskgolf/infinite-discs/c-blend.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/innova-special-stamps.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/dx.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/xt.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/pro.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/r-pro.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/champion.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/blizzard-champion.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/halo-star.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/star.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/g-star.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/starlite.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/overmold.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/glow.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/echo-star.html",
-  #   "https://gurudiscgolf.no/diskgolf/innova/i-dye.html",
-  #   "https://gurudiscgolf.no/diskgolf/sune-sport.html",
-  #   "https://gurudiscgolf.no/diskgolf/latitude-64/zero-burst.html",
-  #   "https://gurudiscgolf.no/diskgolf/latitude-64/retro.html",
-  #   "https://gurudiscgolf.no/diskgolf/latitude-64/zero-line.html",
-  #   "https://gurudiscgolf.no/diskgolf/latitude-64/opto-line.html",
-  #   "https://gurudiscgolf.no/diskgolf/latitude-64/opto-air.html",
-  #   "https://gurudiscgolf.no/diskgolf/latitude-64/opto-x.html",
-  #   "https://gurudiscgolf.no/diskgolf/latitude-64/gold-line.html",
-  #   "https://gurudiscgolf.no/diskgolf/latitude-64/k2-opto-g.html",
-  #   "",
-  #   "",
-  #   "",
-  #   "",
-  #   "",
-  #   "",
-  #   "",
-  #   "",
-  #   "",
-  #   "",
-  #   "",
-  #   "",
-  #   "",
-  # ]
   start_urls = ["https://gurudiscgolf.no/diskgolf.html"]
 
   def parse(self, response):
-    subcategories = response.css(".row.subcategories div").getall()
+    has_subcategories = len(response.css(".row.subcategories div").getall()) > 0
 
-    if len(subcategories) > 0:
+    if has_subcategories:
       for category in response.css(".row.subcategories div"):
         next_page = category.css("a::attr(href)").get()
 
