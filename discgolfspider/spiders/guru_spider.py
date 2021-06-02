aac6c24b6390e8e5b19b37c21600932d54ca679e
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2021-06-02 14:19:23 +0200

    Added more data to disc item for guru

--- discgolfspider/spiders/guru_spider.py
+++ discgolfspider/spiders/guru_spider.py
@@ -8,28 +8,42 @@ class GuruSpider(scrapy.Spider):
   start_urls = ["https://gurudiscgolf.no/diskgolf.html"]
 
   def parse(self, response):
+      for brand in response.css(".row.subcategories div"):
+        next_page = brand.css("a::attr(href)").get(1)
+        brand_name = brand.css("a::text").get(1)
+
+        if next_page is not None:
+          yield response.follow(next_page, callback=self.parse_products, cb_kwargs={"brand": brand_name})
+   
+   
+  def parse_products(self, response, brand):
     has_subcategories = len(response.css(".row.subcategories div").getall()) > 0
 
     if has_subcategories:
       for category in response.css(".row.subcategories div"):
         next_page = category.css("a::attr(href)").get()
+        
 
         if next_page is not None:
-          yield response.follow(next_page, callback=self.parse)
-
+          yield response.follow(next_page, callback=self.parse_products, cb_kwargs={"brand": brand})
     else:
       for product in response.css(".product-layout"):
-        disc_name = product.css(".img-responsive::attr(alt)").get()
-        disc_image = product.css(".img-responsive::attr(src)").get()
-        in_stock = product.css(".stock-status::text").get() != "Utsolgt"
-        disc_url = product.css("a::attr(href)").get()
-        
         disc = DiscItem()
-        disc["name"] = disc_name
-        disc["image"] = disc_image
+        disc["name"] = product.css(".img-responsive::attr(alt)").get()
+        disc["image"] = product.css(".img-responsive::attr(src)").get()
+        disc["in_stock"] = product.css(".stock-status::text").get() != "Utsolgt"
+        disc["url"] = product.css("a::attr(href)").get()
         disc["spider_name"] = self.name
-        disc["in_stock"] = in_stock
-        disc["url"] = disc_url
         disc["retailer"] = self.allowed_domains[0]
+        disc["brand"] = brand
+        
+        price = product.css(".price::text").get()
+        flight_specs = product.css(".attribute-groups > span::text").getall()
 
+        if price:
+          disc["price"] = ''.join(filter(str.isdigit, price))
+
+        if len(flight_specs) == 4:
+          disc["speed"], disc["glide"], disc["turn"], disc["fade"] = flight_specs
+        
         yield disc 
