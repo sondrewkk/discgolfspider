7f44f16ce0aad7b772edef07220c166b1bf365a9
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2021-06-02 12:32:21 +0200

    Default values for env vars

--- discgolfspider/settings.py
+++ discgolfspider/settings.py
@@ -90,17 +90,15 @@ ITEM_PIPELINES = {
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 # MongoDB configuration
-MONGO_HOST = os.getenv("MONGO_HOST", "host")
+MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
 MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
-MONGO_DB = os.getenv("MONGO_DB", "database")
-MONGO_NON_ROOT_USERNAME = os.getenv("MONGO_NON_ROOT_USERNAME", "user")
+MONGO_DB = os.getenv("MONGO_DB", "discinstock")
+MONGO_NON_ROOT_USERNAME = os.getenv("MONGO_NON_ROOT_USERNAME")
 MONGO_NON_ROOT_PASSWORD_FILE = os.getenv("MONGO_NON_ROOT_PASSWORD_FILE")
 
 if MONGO_NON_ROOT_PASSWORD_FILE:
     with open(MONGO_NON_ROOT_PASSWORD_FILE, "r") as file:
         MONGO_NON_ROOT_PASSWORD = file.read()
-else:
-    MONGO_NON_ROOT_PASSWORD = "Passw0rd"
 
 # Crawl interval
 CRAWL_INTERVAL = int(os.getenv("CRAWL_INTERVAL", 3600)) # One hour default
