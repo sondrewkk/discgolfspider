1e0614e6d72154ebdffd5e836e9871c625374f5a
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2022-03-04 22:16:06 +0100

    Using shop endpoint to parse disc

--- discgolfspider/spiders/spinnvilldg_spider.py
+++ discgolfspider/spiders/spinnvilldg_spider.py
@@ -10,42 +10,41 @@ class SpinnvilldgSpider(scrapy.Spider):
     name = "spinnvilldg"
     allowed_domains = ["spinnvilldg.no"]
     start_urls = [
-        "https://www.spinnvilldg.no/thought-space-athletics",
-        "https://www.spinnvilldg.no/prodigy",
-        "https://www.spinnvilldg.no/toby-dyes"
+        "https://www.spinnvilldg.no/shop?Merke=Thought+Space+Athletics&page=1",
+        "https://www.spinnvilldg.no/shop?Merke=Prodigy&page=1",
     ]
 
     def parse(self, response):
-        load_button = response.css('button[data-hook="load-more-button"]').get()
+        for product in response.css("li[data-hook*=\"product-list-grid-item\"]"):
+            disc = CreateDiscItem()
+            disc["name"] = product.css("h3::text").get().replace("[", "").replace("]", "")
+            disc["image"] = product.css("div[data-hook*=\"product-item-images\"]::attr(style)").get().split("(", 1)[1].split(")", 1)[0]
+            disc["url"] = product.css("a::attr(href)").get()
+            disc["spider_name"] = self.name
+            disc["retailer"] = self.allowed_domains[0]
+            disc["brand"] = response.url.split("Merke=")[1].split("&")[0].replace("+", " ")
+            disc["speed"] = None
+            disc["glide"] = None
+            disc["turn"] = None
+            disc["fade"] = None
 
-        if load_button:
-            next_page = response.url
-            parsed_url = urlparse(response.url)
-            query = parsed_url.query
-            
-            if not query:
-                next_page += "?page=2"
-            else:
-                page = int(parse_qs(query)["page"][0]) + 1
-                next_page = response.url.split("=")[0] + f"={page}"
-            
-            yield response.follow(next_page, callback=self.parse)
-        else:
-            for product in response.css("li[data-hook*=\"product-list-grid-item\"]"):
-                disc = CreateDiscItem()
-                disc["name"] = product.css("h3::text").get().replace("[", "").replace("]", "")
-                disc["image"] = product.css('div[data-hook*="product-item-images"]::attr(style)').get().split('(', 1)[1].split(')', 1)[0]
-                disc["url"] = product.css('a::attr(href)').get()
-                disc["spider_name"] = self.name
+            out_of_stock = response.css("span[data-hook=\"product-item-out-of-stock\"]")
+
+            if not out_of_stock:
                 disc["in_stock"] = True
-                disc["retailer"] = self.allowed_domains[0]
-                disc["brand"] = response.url.split(".no/")[1].split("?")[0].replace("-", " ")
-                disc["speed"] = None
-                disc["glide"] = None
-                disc["turn"] = None
-                disc["fade"] = None
-
-                price = product.css('span[data-hook="product-item-price-to-pay"]::text').get()
+                
+                price = product.css("span[data-hook=\"product-item-price-to-pay\"]::text").get()
                 disc["price"] = int(''.join(filter(str.isdigit, price.split(",")[0])))
 
                 yield disc
+            
+        next_page_active = response.css("a[data-hook=next]::attr(aria-disabled)").get() == "false"
+        
+        if next_page_active:
+            url_splitted = response.url.rsplit("=", 1)
+            print(url_splitted)
+            page = int(url_splitted[1]) + 1
+            next_page = f"{url_splitted[0]}={page}"
+
+            yield response.follow(next_page, callback=self.parse)
+ 
