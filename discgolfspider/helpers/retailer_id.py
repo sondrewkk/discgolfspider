b6bfffa8e57e9938bdbd9552ac1ff6aa6236f706
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2022-03-21 00:41:40 +0100

    Brandpipline should handle no brand

--- discgolfspider/helpers/retailer_id.py
+++ discgolfspider/helpers/retailer_id.py
@@ -2,7 +2,7 @@ import hashlib
 
 def create_retailer_id(brand: str, url: str) -> str:
     if not brand or not url:
-        raise ValueError()
+        return None
 
     id = f"{url}{brand}".encode()
     hashed_id = hashlib.md5(id).hexdigest()
