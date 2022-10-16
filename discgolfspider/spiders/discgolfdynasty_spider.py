c490390ed89f3affca1d0fc8f3523d13baf4c6c1
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2022-10-16 20:57:42 +0200

    Discgolfdynasty: Fix an new line in flight specs

--- discgolfspider/spiders/discgolfdynasty_spider.py
+++ discgolfspider/spiders/discgolfdynasty_spider.py
@@ -28,28 +28,33 @@ class DiscgolfdynastySpider(scrapy.Spider):
     def parse_products(self, response, brand):
         for product in response.css(".product-grid-item"):
             disc = CreateDiscItem()
-            disc["name"] = product.css("p::text").get().split(" ", 1)[1]
-            disc["image"] = product.css("img::attr(src)").get()
-            disc["spider_name"] = self.name
-            disc["brand"] = brand
-            disc["retailer"] = self.allowed_domains[0]
-            disc["price"] = int(product.css("small::text").get())
+            
+            try:  
+                disc["name"] = product.css("p::text").get().split(" ", 1)[1]
+                disc["image"] = product.css("img::attr(src)").get()
+                disc["spider_name"] = self.name
+                disc["brand"] = brand
+                disc["retailer"] = self.allowed_domains[0]
+                disc["price"] = int(product.css("small::text").get())
 
-            sold_out = product.css(".badge--sold-out").get()
-            disc["in_stock"] = True if not sold_out else False
+                sold_out = product.css(".badge--sold-out").get()
+                disc["in_stock"] = True if not sold_out else False
 
-            url = product.css("a::attr(href)").get()
-            disc["url"] = f"{self.start_urls[0]}{url}"
-            disc["retailer_id"] = create_retailer_id(brand, url)
+                url = product.css("a::attr(href)").get()
+                disc["url"] = f"{self.start_urls[0]}{url}"
+                disc["retailer_id"] = create_retailer_id(brand, url)
 
-            if not disc["image"]:
-                disc["image"] = "https://via.placeholder.com/300"
+                if not disc["image"]:
+                    disc["image"] = "https://via.placeholder.com/300"
 
-            yield response.follow(
-                disc["url"],
-                callback=self.parse_product_details,
-                cb_kwargs={"disc": disc},
-            )
+                yield response.follow(
+                    disc["url"],
+                    callback=self.parse_product_details,
+                    cb_kwargs={"disc": disc},
+                )
+            except Exception as e:
+                self.logger.error(f"Error parsing disc: {disc['name']}({disc['url']})")
+                self.logger.error(e)
 
         next_page = response.css(".pagination-custom a[title='Neste »']::attr(href)").get()
 
@@ -58,9 +63,11 @@ class DiscgolfdynastySpider(scrapy.Spider):
             yield response.follow(next_page, callback=self.parse_products, cb_kwargs={"brand": brand})
 
     def parse_product_details(self, response, disc):
+        
         flight_specs = response.css(".product-description ul li::text").getall()
 
         if flight_specs:
+            flight_specs = [s for s in flight_specs if s != "\n"]
             flight_specs = flight_specs[:4]
             flight_specs = [self.format_flight_spec(numeric_string) for numeric_string in flight_specs]
             disc["speed"], disc["glide"], disc["turn"], disc["fade"] = flight_specs
@@ -71,4 +78,5 @@ class DiscgolfdynastySpider(scrapy.Spider):
         yield disc
 
     def format_flight_spec(self, flight_spec) -> float:
+        self.logger.debug(f"flight_spec: {flight_spec}")
         return float(flight_spec.split(":")[1].strip())
