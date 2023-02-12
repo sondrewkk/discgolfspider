106c9cbacf29a9dd9783fce447bf6023daf3a931
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2023-02-12 12:55:16 +0100

    Discoverdiscs: Added try to catch exceptions

--- discgolfspider/spiders/discoverdiscs_spider.py
+++ discgolfspider/spiders/discoverdiscs_spider.py
@@ -24,46 +24,53 @@ class DiscoverdiscsSpider(scrapy.Spider):
                 )
 
     def parse_products(self, response, brand):
-        products = response.css("li.grid__item")
         
-        for product in products:
-            disc = CreateDiscItem()
-            name = product.css("span.card-information__text::text").get().replace("\n", "").strip()
+        try:
+            products = response.css("li.grid__item")
+        
+            for product in products:
+                disc = CreateDiscItem()
+                name = product.css("span.card-information__text::text").get().replace("\n", "").strip()
+                url = product.css("a").attrib["href"]
+                url = f"{self.start_urls[0]}{url}"
+                disc["url"] = url
+                disc["retailer_id"] = create_retailer_id(brand, url)
 
-            if self.is_backpack(name):
-                self.logger.info(f"Skipping backpack: {name}")
-                continue
+                if self.is_backpack(name, url):
+                    self.logger.info(f"Skipping backpack: {name}")
+                    continue
 
-            disc["name"] = name
-            disc["image"] = product.css("img").attrib["src"]
-            disc["spider_name"] = self.name
-            
-            badge_text = product.css(".badge::text").get()
-            disc["in_stock"] = True if badge_text != "Utsolgt" else False
-            disc["retailer"] = self.allowed_domains[0]
-            disc["brand"] = brand  
+                disc["name"] = name
+                disc["image"] = product.css("img").attrib["src"]
+                disc["spider_name"] = self.name
+                
+                badge_text = product.css(".badge::text").get()
+                disc["in_stock"] = True if badge_text != "Utsolgt" else False
+                disc["retailer"] = self.allowed_domains[0]
+                disc["brand"] = brand  
 
-            url = product.css("a").attrib["href"]
-            url = f"{self.start_urls[0]}{url}"
-            disc["url"] = url
-            disc["retailer_id"] = create_retailer_id(brand, url)
+                prices = product.css(".price-item::text").getall()
+                disc["price"] = int(prices[len(prices) - 1].split(",")[0])
 
-            prices = product.css(".price-item::text").getall()
-            disc["price"] = int(prices[len(prices) - 1].split(",")[0])
+                flight_specs = product.css(".disc-info__value::text").getall()
 
-            flight_specs = product.css(".disc-info__value::text").getall()
+                if flight_specs and len(flight_specs) == 4:
+                    disc["speed"], disc["glide"], disc["turn"], disc["fade"] = [float(spec) for spec in flight_specs]
+                else:
+                    self.logger.warning(f"{disc['name']}({disc['url']}) is missing flight spec data. {flight_specs=} ")
+                    disc["speed"], disc["glide"], disc["turn"], disc["fade"] = [None, None, None, None]
 
-            if flight_specs and len(flight_specs) == 4:
-                disc["speed"], disc["glide"], disc["turn"], disc["fade"] = [float(spec) for spec in flight_specs]
-            else:
-                self.logger.warning(f"{disc['name']}({disc['url']}) is missing flight spec data. {flight_specs=} ")
-                disc["speed"], disc["glide"], disc["turn"], disc["fade"] = [None, None, None, None]
-
-            yield disc
+                yield disc
+        except Exception as e:
+            self.logger.error(f"Error parsing disc: {disc['name']}({disc['url']})")
+            self.logger.error(e)
 
         next_page = response.css("a.pagination__item--prev::attr(href)").get()
         if next_page is not None:
              yield response.follow(next_page, callback=self.parse_products, cb_kwargs={"brand": brand})
 
-    def is_backpack(self, name):
-        return "bag" in name.lower() or "backpack" in name.lower()
+    def is_backpack(self, name, url):
+        url_contains = "bag" in url or "backpack" in url
+        name_contains = "bag" in name.lower() or "backpack" in name.lower()
+
+        return url_contains or name_contains
