801da6c849049a304d00b2f381676d5edce6f562
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2023-10-15 00:15:59 +0200

    Discoverdiscs: Imporved parsing of price and flight specs

--- discgolfspider/spiders/discoverdiscs_spider.py
+++ discgolfspider/spiders/discoverdiscs_spider.py
@@ -1,5 +1,5 @@
-from ..items import CreateDiscItem
-from ..helpers.retailer_id import create_retailer_id
+from discgolfspider.items import CreateDiscItem
+from discgolfspider.helpers.retailer_id import create_retailer_id
 
 import scrapy
 
@@ -24,53 +24,81 @@ class DiscoverdiscsSpider(scrapy.Spider):
                 )
 
     def parse_products(self, response, brand):
-        
-        try:
-            products = response.css("li.grid__item")
-        
-            for product in products:
-                disc = CreateDiscItem()
+        products = response.css("li.grid__item")
+
+        for product in products:
+            disc = CreateDiscItem()
+
+            try:
                 name = product.css("span.card-information__text::text").get().replace("\n", "").strip()
+                disc["name"] = name
+
                 url = product.css("a").attrib["href"]
                 url = f"{self.start_urls[0]}{url}"
                 disc["url"] = url
                 disc["retailer_id"] = create_retailer_id(brand, url)
 
                 if self.is_backpack(name, url):
-                    self.logger.info(f"Skipping backpack: {name}")
+                    self.logger.debug(f"Skipping backpack: {disc}")
                     continue
 
-                disc["name"] = name
                 disc["image"] = product.css("img").attrib["src"]
                 disc["spider_name"] = self.name
-                
+
                 badge_text = product.css(".badge::text").get()
                 disc["in_stock"] = True if badge_text != "Utsolgt" else False
                 disc["retailer"] = self.allowed_domains[0]
-                disc["brand"] = brand  
+                disc["brand"] = brand
+                disc["price"] = self.parse_price(product)
 
-                prices = product.css(".price-item::text").getall()
-                disc["price"] = int(prices[len(prices) - 1].split(",")[0])
+                flight_specs = self.parse_flight_spec(product)
+                if any(flight_spec is None for flight_spec in flight_specs):
+                    self.logger.warning(f"{disc} is missing flight spec data. {flight_specs=} ")
 
-                flight_specs = product.css(".disc-info__value::text").getall()
-
-                if flight_specs and len(flight_specs) == 4:
-                    disc["speed"], disc["glide"], disc["turn"], disc["fade"] = [float(spec) for spec in flight_specs]
-                else:
-                    self.logger.warning(f"{disc['name']}({disc['url']}) is missing flight spec data. {flight_specs=} ")
-                    disc["speed"], disc["glide"], disc["turn"], disc["fade"] = [None, None, None, None]
+                disc["speed"], disc["glide"], disc["turn"], disc["fade"] = flight_specs
 
                 yield disc
-        except Exception as e:
-            self.logger.error(f"Error parsing disc: {disc['name']}({disc['url']})")
-            self.logger.error(e)
+            except Exception as e:
+                self.logger.error(f"Error parsing disc: {disc}: {e})")
 
         next_page = response.css("a.pagination__item--prev::attr(href)").get()
         if next_page is not None:
-             yield response.follow(next_page, callback=self.parse_products, cb_kwargs={"brand": brand})
+            yield response.follow(
+                next_page,
+                callback=self.parse_products,
+                cb_kwargs={"brand": brand}
+            )
+
+    def parse_flight_spec(self, product) -> tuple:
+        speed = glide = turn = fade = None
+        flight_specs = product.css(".disc-info__value::text").getall()
+
+        if flight_specs and len(flight_specs) == 4:
+            speed, glide, turn, fade = [float(spec) for spec in flight_specs]
+
+        return speed, glide, turn, fade
 
     def is_backpack(self, name, url):
         url_contains = "bag" in url or "backpack" in url
         name_contains = "bag" in name.lower() or "backpack" in name.lower()
 
         return url_contains or name_contains
+
+    def parse_price(self, product) -> int:
+        price = -1
+        prices = product.css(".price-item::text").getall()
+        prices = [self.format_price(price) for price in prices]
+        prices = [price for price in prices if price != ""]
+
+        if prices:
+            price = int(prices[1])
+
+        return price
+
+    def format_price(self, price: str) -> str:
+        price_formatted = price.replace("\n", "").replace("Fra", "").strip()
+
+        if "," in price_formatted:
+            price_formatted = price_formatted.split(",")[0]
+
+        return price_formatted
